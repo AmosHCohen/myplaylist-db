60e43375d31a8f515f6a03ec8c76f5c437104d6a
AmosCohen <amoshirac@gmail.com>
2022-07-12 17:37:41 +0300

    merge with main branch

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config()
-const express = require('express'),
-    app = express(),
-    PORT = process.env.PORT || 3001,
-    router = require('./Routes')
-
-app.use(express.json())
-app.use(require('cors')())
-
-app.use('/api', router)
-
-app.listen(PORT, () => console.log(`the server is running => ${PORT}`))
-require('./DL/db').connect()
-=======
 require('dotenv').config()
 const express = require('express'),
     app = express(),
@@ -28,4 +13,3 @@ app.use("/api", router)
 
 app.listen(PORT, () => console.log(`server is running => ${PORT}`))
 require('./DL/db').connect();
->>>>>>> refs/remotes/origin/main
